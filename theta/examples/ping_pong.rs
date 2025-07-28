16f543e90e3a482f1d7d08338c4f0ba83bb1ca12
Chanwoo Ahn <cwahn@arsvivendi.io>
2025-07-28 13:37:32 +0900

    chore: Draft connection part with no good

--- theta/examples/ping_pong.rs
+++ theta/examples/ping_pong.rs
@@ -1,3 +1,6 @@
+use std::str::FromStr;
+
+use iroh::PublicKey;
 use theta::prelude::GlobalContext;
 use tracing::{error, info};
 use tracing_subscriber::fmt::time::ChronoLocal;
@@ -19,6 +22,28 @@ async fn main() -> anyhow::Result<()> {
     info!("GlobalContext initialized with public key: {public_key}");
 
     // Ask for user of other peer's public key
+    info!("Please enter the public key of the other peer:");
+
+    let mut input = String::new();
+    let other_public_key = loop {
+        std::io::stdin().read_line(&mut input)?;
+        let trimmed = input.trim();
+        if trimmed.is_empty() {
+            error!("Public key cannot be empty. Please try again.");
+            input.clear();
+            continue;
+        }
+
+        match PublicKey::from_str(trimmed) {
+            Ok(key) => break key,
+            Err(e) => {
+                error!("Invalid public key format: {e}");
+                input.clear();
+            }
+        };
+    };
+
+    g_ctx.connect_peer(other_public_key).await?;
 
     Ok(())
 }
