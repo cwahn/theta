8f61613d6e2ae982cbb5d01ade9a92f1f745b525
Chanwoo Ahn <cwahn@arsvivendi.io>
2025-09-14 16:54:24 +0900

    refactor: Rename SaveSnapshotExt to PersistentContextExt

--- theta/src/persistence/mod.rs
+++ theta/src/persistence/mod.rs
@@ -102,5 +102,5 @@ pub mod storages;
 
 // Re-exports
 pub use persistent_actor::{
-    PersistenceError, PersistentActor, PersistentSpawnExt, PersistentStorage, SaveSnapshotExt,
+    PersistenceError, PersistentActor, PersistentSpawnExt, PersistentStorage, PersistentContextExt,
 };
